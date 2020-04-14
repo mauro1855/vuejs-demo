353d4013974405d4ad995bfa732ec43403825e4e
mauro1855 <mauro1855@live.com.pt>
2020-04-14 23:36:37 +0200

    Fixed Travis - Heroku integration

---
+++ server.js
@@ -0,0 +1,14 @@
+var path = require('path');
+var express = require('express');
+
+var app = express();
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('*', (request, response) => {
+	response.sendFile(path.join(__dirname, 'dist', 'index.html'))});
+
+app.set('port', process.env.PORT || 9000);
+var server = app.listen(app.get('port'), function() {
+  console.log('listening on port ', server.address().port);
+});
